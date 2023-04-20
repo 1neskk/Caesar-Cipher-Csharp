cd1cad74cfd0574631b93a0b971be42ac1483efe
1nullptr <129955496+1nullptr@users.noreply.github.com>
2023-04-20 11:12:49 -0300

    Initial Commit

---
+++ Test/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static string Encrypt(string text, int s)
+    {
+        string result = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (Char.IsUpper(text[i]))
+            {
+                result += (char)(((int)text[i] + s - 65) % 26 + 65);
+            }
+            else
+            {
+                result += (char)(((int)text[i] + s - 97) % 26 + 97);
+            }
+        }
+        return result;
+    }
+
+    static string Decrypt(string text, int s)
+    {
+        string result = "";
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (Char.IsUpper(text[i]))
+            {
+                result += (char)(((int)text[i] - s - 65) % 26 + 65);
+            }
+            else
+            {
+                result += (char)(((int)text[i] - s - 97) % 26 + 97);
+            }
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        string text = "ATTACKATONCE";
+        int s = 5;
+        Console.WriteLine("Text : " + text);
+        Console.WriteLine("Shift : " + s);
+        string encryptedText = Encrypt(text, s);
+        Console.WriteLine("Encrypted Text : " + encryptedText);
+        Console.WriteLine("Decrypted Text : " + Decrypt(encryptedText, s));
+    }
+}
